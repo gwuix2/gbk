efeb379e29d660fbd4d90e884b318a8aed5889c7
gwuix2 <coolreed@gmail.com>
2013-09-01 17:32:53 +0200

    fix?

--- db/migrate/20130828122014_add_slug_index_to_posts.rb
+++ db/migrate/20130828122014_add_slug_index_to_posts.rb
@@ -1,6 +1,6 @@
 class AddSlugIndexToPosts < ActiveRecord::Migration
   def change
-  	add_column :posts, :slug, :String
+  	add_column :posts, :slug, :string
   	add_index :posts, :slug, unique: true
   end
 end
